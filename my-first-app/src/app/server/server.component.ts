055b8e2cd2494f61c9a02d612d139d523cacf8ef
Jason Cameron <dev.jasoncameron@gmail.com>
2022-07-08 20:34:12 -0500

    broke on rel url. working

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: "ServerComponent",
+  selector: 'app-server',
+  templateUrl: 'server/server.component.ts'
+})
+export class ServerComponent {
+
+}
+
