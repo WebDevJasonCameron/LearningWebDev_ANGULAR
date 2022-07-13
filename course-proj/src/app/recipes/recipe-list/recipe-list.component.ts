916ce3e027afa4b8ad1d539251519c2fab872562
Jason Cameron <dev.jasoncameron@gmail.com>
2022-07-13 14:35:11 -0500

    recipe array dynam shown in html

--- course-proj/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-proj/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,13 @@ export class RecipeListComponent implements OnInit {
 
   // ATT
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'Simply Test Desc', 'https://unsplash.com/images/food')
+    new Recipe('A Test Recipe', 
+              'Simply Test Desc', 
+              'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'),
+              
+    new Recipe('A Test Recipe', 
+              'Simply Test Desc', 
+              'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'),
   ];
 
   // CON
