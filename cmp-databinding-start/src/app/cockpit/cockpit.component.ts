3579cd35b6f721b09079f69e43773f40f104d03e
Jason Cameron <dev.jasoncameron@gmail.com>
2022-07-15 22:27:34 -0500

    comp same thing but with ViewChild instead of local ref temp

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -12,7 +12,8 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated')blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
   
   // CON
   constructor() { }
@@ -23,17 +24,17 @@ export class CockpitComponent implements OnInit {
 
   // FUN
   onAddServer(nameInput: HTMLInputElement) {
-    console.log(nameInput.value)
+    
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
