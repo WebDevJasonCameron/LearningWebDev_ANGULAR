0d750e7d8b6a4aea2f0a1d9d37e1411fac8f7f41
Jason Cameron <dev.jasoncameron@gmail.com>
2022-07-17 20:55:13 -0500

    adding "add task" functionality

--- task-tracker/src/app/components/tasks/tasks.component.ts
+++ task-tracker/src/app/components/tasks/tasks.component.ts
@@ -31,7 +31,9 @@ export class TasksComponent implements OnInit {
     this.taskService.updateTaskReminder(task).subscribe()
   }
 
-
+  addTask(task: Task){
+    console.log(task)
+  }
 
 
 }  //<--END
