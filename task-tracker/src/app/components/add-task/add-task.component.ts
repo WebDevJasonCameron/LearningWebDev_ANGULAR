37d28f4477bab315258de59d95212d1180f9dcdd
Jason Cameron <dev.jasoncameron@gmail.com>
2022-07-17 21:36:08 -0500

    add task form display toggle fully active

--- task-tracker/src/app/components/add-task/add-task.component.ts
+++ task-tracker/src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -14,9 +16,15 @@ export class AddTaskComponent implements OnInit {
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
 
   // CON
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription =  this.uiService
+    .onToggle()
+    .subscribe((value) => (this.showAddTask = value))
+  }
 
   // LC
   ngOnInit(): void {
