2a97c441c27594759685abad6f64c358c3598a62
Jason Cameron <dev.jasoncameron@gmail.com>
2022-07-19 17:50:20 -0500

    comp test api access, add compo list

---
+++ add-tuts/pokemon-app-01/src/app/services/data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  // ATT
+
+  // CON
+  constructor(
+    private http: HttpClient 
+  ) { }
+
+  // LC
+
+
+  // FUN
+  getPokemons(){
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=10"`)
+  }
+
+
+} //<--END
