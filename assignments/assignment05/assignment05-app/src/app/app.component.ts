2ae6e2c48b660b7e7cd7712257819f9725e8fdb1
Jason Cameron <dev.jasoncameron@gmail.com>
2022-11-01 20:29:42 -0500

    add odd nums to show on page

--- assignments/assignment05/assignment05-app/src/app/app.component.ts
+++ assignments/assignment05/assignment05-app/src/app/app.component.ts
@@ -5,8 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
   onIntervalFired(firedNumber: number) {
-    console.log(firedNumber);
+    if(firedNumber % 2 === 0){
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber)
+    }
   }
 }
--- assignments/assignment05/assignment05-app/src/app/odd/odd.component.ts
+++ assignments/assignment05/assignment05-app/src/app/odd/odd.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-odd',
   templateUrl: './odd.component.html',
   styleUrls: ['./odd.component.css']
 })
+
 export class OddComponent implements OnInit {
 
+  // VAR
+  @Input() number: number;
+
+
+  // CON
   constructor() { }
 
+  // FUN
   ngOnInit(): void {
   }
 
-}
+}  //<- END
