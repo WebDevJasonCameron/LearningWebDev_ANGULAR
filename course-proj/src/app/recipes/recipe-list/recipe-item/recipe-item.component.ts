e0dc6aaf43ac2b8b93f1e491515d8814df2c4ca8
Jason Cameron <dev.jasoncameron@gmail.com>
2022-11-03 18:33:57 -0500

    moved html for recipe (previously in recipe-list.html) into its own sub-component

--- course-proj/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ course-proj/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
+  // VAR
+  @Input() recipe: Recipe;
+
+  // CON
   constructor() { }
 
+  // FUN
   ngOnInit(): void {
   }
 
